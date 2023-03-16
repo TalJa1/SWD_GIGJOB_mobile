ed841bf240e679387b85935c546453a66f93dd11
TalJa1 <91865008+TalJa1@users.noreply.github.com>
2023-03-16 10:53:59 +0700

    feat: register worker

--- lib/DTO/WorkerDTO.dart
+++ lib/DTO/WorkerDTO.dart
@@ -1,36 +1,60 @@
 class WorkerDTO {
+  String? accountId;
   String? id;
   String? firstName;
   String? lastName;
   String? middleName;
   String? birthday;
   String? education;
+  String? email;
+  String? phone;
+  String? username;
+  String? password;
+  String? imageUrl;
 
   WorkerDTO(
-      {this.id,
+      {this.accountId,
+      this.id,
       this.firstName,
       this.lastName,
       this.middleName,
       this.birthday,
-      this.education});
+      this.education,
+      this.email,
+      this.phone,
+      this.username,
+      this.password,
+      this.imageUrl});
 
   WorkerDTO.fromJson(Map<String, dynamic> json) {
+    accountId = json['accountId'];
     id = json['id'];
     firstName = json['firstName'];
     lastName = json['lastName'];
     middleName = json['middleName'];
     birthday = json['birthday'];
     education = json['education'];
+    email = json['email'];
+    phone = json['phone'];
+    username = json['username'];
+    password = json['password'];
+    imageUrl = json['imageUrl'];
   }
 
   Map<String, dynamic> toJson() {
     final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['accountId'] = this.accountId;
     data['id'] = this.id;
     data['firstName'] = this.firstName;
     data['lastName'] = this.lastName;
     data['middleName'] = this.middleName;
     data['birthday'] = this.birthday;
     data['education'] = this.education;
+    data['email'] = this.email;
+    data['phone'] = this.phone;
+    data['username'] = this.username;
+    data['password'] = this.password;
+    data['imageUrl'] = this.imageUrl;
     return data;
   }
 }
--- lib/view/register_Worker.dart
+++ lib/view/register_Worker.dart
@@ -2,9 +2,14 @@
 
 import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:gigjob_mobile/DTO/WorkerDTO.dart';
+import 'package:gigjob_mobile/viewmodel/register_viewmodel.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:gigjob_mobile/services/UploadFile_service.dart';
+import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
 import 'dart:math';
+
+import 'package:intl/intl.dart';
 
 class RegisterWorkerPage extends StatefulWidget {
   const RegisterWorkerPage({Key? key}) : super(key: key);
@@ -17,7 +22,10 @@ class _RegisterWorkerPageState extends State<RegisterWorkerPage> {
   @override
   void initState() {
     super.initState();
+    registerViewModel = RegisterViewModel();
   }
+
+  late RegisterViewModel registerViewModel;
 
   final ImagePicker picker = ImagePicker();
   XFile? uploadfile;
@@ -27,6 +35,11 @@ class _RegisterWorkerPageState extends State<RegisterWorkerPage> {
   final _textnameController = TextEditingController();
   final _textdiplomaController = TextEditingController();
   final _textbirtdayController = TextEditingController();
+  final _textfirstController = TextEditingController();
+  final _textmidleController = TextEditingController();
+  final _textlastController = TextEditingController();
+  final _textpassController = TextEditingController();
+  final _textphoneController = TextEditingController();
 
   Future pickImg() async {
     // ignore: unused_local_variable
@@ -40,58 +53,73 @@ class _RegisterWorkerPageState extends State<RegisterWorkerPage> {
     print(uploadfile);
   }
 
+  String? _validatePhoneNumber(String? value) {
+    String pattern = r'(^(?:[+0]9)?[0-9]{10,12}$)';
+    RegExp regExp = RegExp(pattern);
+    if (value == null || value.isEmpty) {
+      return 'Phone number is required';
+    } else if (!regExp.hasMatch(value)) {
+      return 'Invalid phone number';
+    }
+    return null;
+  }
+
+  DateTime? selectedDate;
+
   @override
   Widget build(BuildContext context) {
     // ignore: prefer_const_constructors
     return Scaffold(
         resizeToAvoidBottomInset: false,
         backgroundColor: Colors.white,
-        body: Column(
-          children: [
-            Stack(
-              // ignore: prefer_const_literals_to_create_immutables
-              children: <Widget>[
-                Positioned(
-                    child: SizedBox(
-                  height: 240,
-                  width: MediaQuery.of(context).size.width,
-                )),
-                // ignore: prefer_const_constructors
-                CustomScrollView(
-                  scrollDirection: Axis.vertical,
-                  shrinkWrap: true,
-                  // ignore: prefer_const_literals_to_create_immutables
-                  slivers: <Widget>[
-                    // ignore: prefer_const_constructors
-                    SliverAppBar(
-                      backgroundColor: const Color.fromARGB(255, 234, 234, 234),
-                      floating: true,
-                    ),
-                    // ignore: prefer_const_constructors
-                    SliverAppBar(
-                      backgroundColor: const Color.fromARGB(255, 234, 234, 234),
-                      floating: true,
-                      flexibleSpace: const FlexibleSpaceBar(
-                        title: Text(
-                          '',
-                          style: TextStyle(color: Colors.black),
-                        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Stack(
+                // ignore: prefer_const_literals_to_create_immutables
+                children: <Widget>[
+                  Positioned(
+                      child: SizedBox(
+                    height: 240,
+                    width: MediaQuery.of(context).size.width,
+                  )),
+                  // ignore: prefer_const_constructors
+                  CustomScrollView(
+                    scrollDirection: Axis.vertical,
+                    shrinkWrap: true,
+                    // ignore: prefer_const_literals_to_create_immutables
+                    slivers: <Widget>[
+                      // ignore: prefer_const_constructors
+                      SliverAppBar(
+                        backgroundColor:
+                            const Color.fromARGB(255, 234, 234, 234),
+                        floating: true,
                       ),
-                    )
-                  ],
-                ),
-                Positioned(right: 20, top: 80, child: userImg()),
-              ],
-            ),
+                      // ignore: prefer_const_constructors
+                      SliverAppBar(
+                        backgroundColor:
+                            const Color.fromARGB(255, 234, 234, 234),
+                        floating: true,
+                        flexibleSpace: const FlexibleSpaceBar(
+                          title: Text(
+                            '',
+                            style: TextStyle(color: Colors.black),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  Positioned(right: 20, top: 80, child: userImg()),
+                ],
+              ),
 
-            Expanded(
-                child: Padding(
-              padding: const EdgeInsets.only(top: 30),
-              child: formEdit(),
-            )),
-            // Expanded(child: )
-            const SizedBox(height: 10)
-          ],
+              Padding(
+                padding: const EdgeInsets.only(top: 30),
+                child: formEdit(),
+              ),
+              // Expanded(child: )
+            ],
+          ),
         ));
   }
 
@@ -139,27 +167,26 @@ class _RegisterWorkerPageState extends State<RegisterWorkerPage> {
 
   Widget formEdit() {
     return Container(
-        height: MediaQuery.of(context).size.height,
-        width: MediaQuery.of(context).size.width,
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const Text(
-                'Welcome to GIGJOB',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold, // to make text bold
-                  fontSize: 24, // to set font size
-                ),
-                textAlign: TextAlign.center, // to center the text
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              textForm(),
-            ],
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Text(
+            'Welcome to GIGJOB',
+            style: TextStyle(
+              fontWeight: FontWeight.bold, // to make text bold
+              fontSize: 24, // to set font size
+            ),
+            textAlign: TextAlign.center, // to center the text
           ),
-        ));
+          const SizedBox(
+            height: 20,
+          ),
+          textForm(),
+        ],
+      ),
+    );
   }
 
   Widget textForm() {
@@ -173,11 +200,11 @@ class _RegisterWorkerPageState extends State<RegisterWorkerPage> {
               controller: _textnameController,
               decoration: const InputDecoration(
                 border: OutlineInputBorder(),
-                labelText: 'Enter your name',
+                labelText: 'Enter your username',
               ),
               validator: (value1) {
                 if (value1!.isEmpty) {
-                  return 'Please enter your name';
+                  return 'Please enter your username';
                 }
                 return null;
               },
@@ -214,14 +241,14 @@ class _RegisterWorkerPageState extends State<RegisterWorkerPage> {
           Padding(
             padding: const EdgeInsets.fromLTRB(16, 16, 16, 0),
             child: TextFormField(
-              controller: _textbirtdayController,
+              controller: _textfirstController,
               decoration: const InputDecoration(
                 border: OutlineInputBorder(),
-                labelText: 'Enter your birthday',
+                labelText: 'Enter your firstname',
               ),
               validator: (value3) {
                 if (value3!.isEmpty) {
-                  return 'Please enter your birthday';
+                  return 'Please enter your firstname';
                 }
                 return null;
               },
@@ -233,16 +260,168 @@ class _RegisterWorkerPageState extends State<RegisterWorkerPage> {
               },
             ),
           ),
-          Container(
-            height: MediaQuery.of(context).size.height / 10,
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16, 16, 16, 0),
+            child: TextFormField(
+              controller: _textmidleController,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Enter your midlename',
+              ),
+              validator: (value4) {
+                if (value4!.isEmpty) {
+                  return 'Please enter your midlename';
+                }
+                return null;
+              },
+              onChanged: (value4) {
+                if (_formKey.currentState!.validate()) {
+                  // The form is valid, do something with the input
+                  print('User entered: $value4');
+                }
+              },
+            ),
           ),
-          uploadbtn(_formKey.currentState),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16, 16, 16, 0),
+            child: TextFormField(
+              controller: _textlastController,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Enter your lastname',
+              ),
+              validator: (value5) {
+                if (value5!.isEmpty) {
+                  return 'Please enter your lastname';
+                }
+                return null;
+              },
+              onChanged: (value5) {
+                if (_formKey.currentState!.validate()) {
+                  // The form is valid, do something with the input
+                  print('User entered: $value5');
+                }
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16, 16, 16, 0),
+            child: TextFormField(
+              controller: _textphoneController,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Enter your phone',
+              ),
+              validator: _validatePhoneNumber,
+              onChanged: (value6) {
+                if (_formKey.currentState!.validate()) {
+                  // The form is valid, do something with the input
+                  print('User entered: $value6');
+                }
+              },
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.fromLTRB(16, 16, 16, 0),
+            child: TextFormField(
+              controller: _textpassController,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Enter your password',
+              ),
+              validator: (value7) {
+                if (value7!.isEmpty) {
+                  return 'Please enter your password';
+                }
+                return null;
+              },
+              onChanged: (value7) {
+                if (_formKey.currentState!.validate()) {
+                  // The form is valid, do something with the input
+                  print('User entered: $value7');
+                }
+              },
+            ),
+          ),
+          Padding(
+              padding: const EdgeInsets.fromLTRB(16, 16, 16, 0),
+              child: TextButton(
+                  onPressed: () {
+                    DatePicker.showDatePicker(context,
+                        showTitleActions: true,
+                        minTime: DateTime(1990, 3, 5),
+                        maxTime: DateTime(2019, 6, 7), onChanged: (date) {
+                      print('change $date');
+                    }, onConfirm: (date) {
+                      setState(() {
+                        selectedDate = date;
+                      });
+                      print('confirm $date');
+                    }, currentTime: DateTime.now(), locale: LocaleType.es);
+                  },
+                  child: Container(
+                    child: Column(
+                      children: [
+                        if (selectedDate != null) ...[
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              // ignore: prefer_const_constructors
+                              Text(
+                                'Your birthday: ',
+                                style: const TextStyle(color: Colors.black),
+                              ),
+                              Text(
+                                '$selectedDate',
+                                style: const TextStyle(color: Colors.black),
+                              )
+                            ],
+                          )
+                        ],
+                        const Text(
+                          'Choose your birthday',
+                          style: TextStyle(color: Colors.blue),
+                        ),
+                      ],
+                    ),
+                  ))),
+          const SizedBox(
+            height: 10,
+          ),
+          uploadbtn(
+              _formKey.currentState,
+              _textfirstController.text,
+              _textlastController.text,
+              _textmidleController.text,
+              _textphoneController.text,
+              _textnameController.text,
+              _textpassController.text,
+              selectedDate.toString(),
+              _textdiplomaController.text),
         ],
       ),
     );
   }
 
-  Widget uploadbtn(FormState? state) {
+  Widget uploadbtn(
+      FormState? state,
+      String first,
+      String last,
+      String middle,
+      String phone,
+      String username,
+      String pass,
+      String birth,
+      String diploma) {
+    WorkerDTO dto = new WorkerDTO();
+    dto.firstName = first;
+    dto.middleName = middle;
+    dto.lastName = last;
+    dto.phone = phone;
+    dto.username = username;
+    dto.password = pass;
+    dto.birthday = birth;
+    dto.education = diploma;
     return Column(
       children: [
         const Text(
@@ -257,9 +436,10 @@ class _RegisterWorkerPageState extends State<RegisterWorkerPage> {
           width: MediaQuery.of(context).size.width,
           padding: const EdgeInsets.only(left: 16, right: 16),
           child: TextButton(
-            onPressed: () {
+            onPressed: () async {
               if (state!.validate()) {
-                UploadFileService().uploadImage(uploadfile!);
+                // UploadFileService().uploadImage(uploadfile!);
+                await registerViewModel.registerWorker(dto);
               } else {}
             },
             style: ButtonStyle(
